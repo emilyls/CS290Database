3b7b8bfbbdae6fdda31319a0e405d2de9642f62b
emily <snyderem@oregonstate.edu>
2015-12-05 04:49:27 +0000

    adding row and getting updated table working

--- DatabaseNode.js
+++ DatabaseNode.js
@@ -19,25 +19,20 @@ var pool = mysql.createPool({
 app.use(express.static(__dirname + '/public'));
 
 app.get('/newWorkout', function(req, res, next) {
+  var context = {};
   pool.query('INSERT INTO workouts (`name`, `reps`, `weight`, `date`, `lbs`) VALUES (?, ?, ?, ?, ?)', [req.query.name, req.query.reps, req.query.weight, req.query.date, req.query.lbs], function(err, result){
     if (err) {
       next(err);
       return;
     }
+    pool.query('SELECT * FROM workouts', function(err, rows, fields) {
+      if (err) {
+        next(err);
+        return;
+      }
+      res.send(JSON.stringify(rows));
+    });
   });
-  rens.render(getData)
-});
-
-app.get('/getData', function(req, res, next) {
-  pool.query('SELECT * FROM workouts', function(err, rows, fields) {
-    var context = {};
-    if (err) {
-      next(err);
-      return;
-    }
-    console.log(JSON.stringify(rows));
-  });
-  
 });
 
 app.get('/reset-table',function(req,res,next){
--- public/database.js
+++ public/database.js
@@ -1,48 +1,36 @@
 document.addEventListener('DOMContentLoaded', bindButton);
-document.addEventListener('DOMContentLoaded', createTable);
-
-function createTable() {
-	var request = new XMLHttpRequest();
-	if (request.readyState == 4 && request.status == 200) {
-		console.log("empty table");
-	}
-	request.open('GET', 'http://52.88.123.171:5000/reset-table', true);
-	request.send(null);
-	event.preventDefault();
-}
 
 function bindButton() {
-	document.getElementById('newWorkout').addEventListener('click', function(event){
-		var request = new XMLHttpRequest();
-		var name = document.getElementById('name').value;
-		var reps = document.getElementById('reps').value;
-		var weight = document.getElementById('weight').value;
-		var date = document.getElementById('date').value;
-		var lbs = document.getElementById('lbs').value;
+  document.getElementById('newWorkout').addEventListener('click', function(event){
+    var request = new XMLHttpRequest();
+    var name = document.getElementById('name').value;
+    var reps = document.getElementById('reps').value;
+    var weight = document.getElementById('weight').value;
+    var date = document.getElementById('date').value;
+    var lbs = document.getElementById('lbs').value;
     if (name == "") {
       console.log("error"); 
       return;
     }
+    request.onreadystatechange = function() {
+      if (request.readyState == 4 && request.status == 200) {
+        var response = JSON.parse(request.responseText);
+        ///var par = document.createElement("p");
+        //var info = document.createTextNode(response);
+        //par.appendChild(info);
+        
+        console.log(response);
+      }
+    }
 
-		
-		request.onreadystatechange = function() {
-			if (request.readyState == 4 && request.status == 200) {
-				var response = JSON.parse(request.responseText);
-				//var data = JSON.parse(response.data);
-				// document.getElementById('dataReturned').textContent = "Name: " + data.userName + "\tAge: " + data.userAge;
-				//console.log(data)
-			}
-		}
-
-		request.open('GET', 'http://52.88.123.171:5000/newWorkout?name=' + name + '&reps=' + reps + '&weight=' + weight + '&date=' + date + '&lbs=' + lbs, true);
-
-		request.send(null);
-		document.getElementById('name').value = "";
-		document.getElementById('reps').value = "";
-		document.getElementById('weight').value = "";
-		document.getElementById('date').value = "";
-		document.getElementById('lbs').value = "";
-		event.preventDefault();
-	});
+    request.open('GET', 'http://52.88.123.171:5000/newWorkout?name=' + name + '&reps=' + reps + '&weight=' + weight + '&date=' + date + '&lbs=' + lbs, true);
+    request.send(null);
+    document.getElementById('name').value = "";
+    document.getElementById('reps').value = "";
+    document.getElementById('weight').value = "";
+    document.getElementById('date').value = "";
+    document.getElementById('lbs').value = "";
+    event.preventDefault();
+  });
 }
 
