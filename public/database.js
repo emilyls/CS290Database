617e5d1aa06447af3477872967d1edbf240fc62a
emilyls <snyderem@oregonstate.edu>
2015-12-05 00:10:46 -0800

    s

--- public/database.js
+++ public/database.js
@@ -30,50 +30,7 @@ function bindButton() {
             j++;
           }
           if (!found) {
-            var name = response[i].name;
-            var date = response[i].date;
-            var lbs = response[i].lbs;
-            var reps = response[i].reps;
-            var weight = response[i].weight;
-            var row = document.createElement("tr");
-            var idCell = document.createElement("td");
-            var nameCell = document.createElement("td");
-            var repsCell = document.createElement("td");
-            var weightCell = document.createElement("td");
-            var dateCell = document.createElement("td");
-            var lbsCell = document.createElement("td");
-            idCell.appendChild(document.createTextNode(id));
-            idCell.className = "id";
-            nameCell.appendChild(document.createTextNode(name));
-            repsCell.appendChild(document.createTextNode(reps));
-            weightCell.appendChild(document.createTextNode(weight));
-            dateCell.appendChild(document.createTextNode(date));
-            lbsCell.appendChild(document.createTextNode(lbs));
-            row.appendChild(idCell);
-            row.appendChild(nameCell);
-            row.appendChild(repsCell);
-            row.appendChild(weightCell);
-            row.appendChild(dateCell);
-            row.appendChild(lbsCell);
-
-            var update = document.createElement("FORM");
-            var formId = document.createElement("P");
-            formId.textContent = id.toString();
-            formId.id = "formId";
-            var editBtn = document.createElement("BUTTON");
-            editBtn.appendChild(document.createTextNode("Edit"));
-            editBtn.id = "edit";
-            var deleteBtn = document.createElement("BUTTON");
-            deleteBtn.appendChild(document.createTextNode("Delete"));
-            deleteBtn.id = "delete";
-            deleteBtn.onclick = deleteRow; 
-            update.appendChild(formId);
-            update.appendChild(editBtn);
-            update.appendChild(deleteBtn);
-            row.appendChild(update);
-
-            var table = document.getElementById('workouts');
-            table.appendChild(row);
+            createRow(response[i]);
           }
         }
       }
@@ -105,47 +62,6 @@ function firstTable() {
 
   request.open('GET', 'newWorkout', true);
   request.send(null);
-}
-
-function deleteRow(event) {
-//  document.getElementById('delete').addEventListener('click', function(event) {
-    var request = new XMLHttpRequest();
-
-    console.log(event);
-    var button = event.target;
-//    var form = event.parentNode;
-//    console.log(form);
-    var found = false;
-//   var row;
-//    while (button.previousSibling.id != "formId") {
-//      formId = button.previousSibling;
-//    }
-
-//    var table = document.getElementById('workouts');
-//    for (var i = 0; i < table.rows.length; i++) {
-//      var cells = table.row[i].childNodes;
-//      var id = cells.getElementById("id");
-//      if (Number(id.innerHTML) == formId) {
-//        row = table.row[i];
-//        found = true;
-//        break;
-//      }
-//    }
-
-//    request.onreadystatechange = function() {
-//      if (request.readyState == 4 && request.status == 200) {
-//        var response = JSON.parse(request.responseText);
-//        console.log(response);
-//      }
-//    }
-
-    if (found == true) {
-      request.open('GET', '/deleteWorkout?id=' + id, true);
-      request.send(null);
-      table.removeChild(row); 
-    }
-    event.preventDefault();
- // });
 }
 
 function createRow(data) {
@@ -186,7 +102,44 @@ function createRow(data) {
         var deleteBtn = document.createElement("BUTTON");
         deleteBtn.appendChild(document.createTextNode("Delete"));
         deleteBtn.id = "delete";
-        deleteBtn.onclick = deleteRow; 
+        deleteBtn..addEventListener('click', function(event) {
+          var request = new XMLHttpRequest();
+
+          console.log(event);
+          var button = event.target;
+      //    var form = event.parentNode;
+      //    console.log(form);
+          var found = false;
+      //   var row;
+      //    while (button.previousSibling.id != "formId") {
+      //      formId = button.previousSibling;
+      //    }
+
+      //    var table = document.getElementById('workouts');
+      //    for (var i = 0; i < table.rows.length; i++) {
+      //      var cells = table.row[i].childNodes;
+      //      var id = cells.getElementById("id");
+      //      if (Number(id.innerHTML) == formId) {
+      //        row = table.row[i];
+      //        found = true;
+      //        break;
+      //      }
+      //    }
+
+      //    request.onreadystatechange = function() {
+      //      if (request.readyState == 4 && request.status == 200) {
+      //        var response = JSON.parse(request.responseText);
+      //        console.log(response);
+      //      }
+      //    }
+
+          if (found == true) {
+            request.open('GET', '/deleteWorkout?id=' + id, true);
+            request.send(null);
+            table.removeChild(row); 
+          }
+          event.preventDefault();
+        }); 
         update.appendChild(formId);
         update.appendChild(editBtn);
         update.appendChild(deleteBtn);
