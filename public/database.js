8b099f7be4c577938b48ad14be526d126ceb2b1a
emilyls <snyderem@oregonstate.edu>
2015-12-05 08:13:07 -0800

    asf

--- public/database.js
+++ public/database.js
@@ -94,7 +94,7 @@ function createRow(data) {
 
   var deleteForm = document.createElement("FORM");
   var deleteFormId = document.createElement("INPUT");
-  deleteFormId.id = 'deleteId';
+  deleteFormId.className = 'deleteId';
   deleteFormId.value = id;
   deleteForm.appendChild(deleteFormId);
   var deleteBtn = document.createElement("BUTTON");
@@ -126,7 +126,7 @@ function deleteRow(id) {
       var response = JSON.parse(request.responseText);
       console.log(response);
       var table = document.getElementById('workouts');
-      var formIds = document.getElementById('deleteId');
+      var formIds = document.getElementsByClassName('deleteId');
       console.log(formIds[0]);
       var i = 0;
       var found = false
